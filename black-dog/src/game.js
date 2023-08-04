e9125235dbfbabdd24a955f4a0b54c69a0582325
dfsdanimaulana <danimaulana9f@gmail.com>
2023-08-04 16:50:57 +0700

    fix bug

--- black-dog/src/game.js
+++ black-dog/src/game.js
@@ -1,17 +1,17 @@
 import Player from './player.js'
 import InputHandler from './input.js'
 
-export default Game {
-  constructor(width, height) {
-    this.width = width
-    this.height = height
-    this.player = new Player(this)
-    this.input = new InputHandler(this)
-  }
-  update(deltaTime) {
-    this.player.update(deltaTime)
-  }
-  draw(ctx) {
-    this.player.draw(ctx)
-  }
+export default class Game {
+    constructor(width, height) {
+        this.width = width
+        this.height = height
+        this.player = new Player(this)
+        this.input = new InputHandler(this)
+    }
+    update(deltaTime) {
+        this.player.update(deltaTime)
+    }
+    draw(ctx) {
+        this.player.draw(ctx)
+    }
 }
