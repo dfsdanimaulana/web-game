640b8cbaa0aa9f5a7342be574cfe534822ee28fa
dfsdanimaulana <danimaulana9f@gmail.com>
2023-08-12 10:57:12 +0700

    create dynamic terrains

--- tank-war/src/terrains/elements.js
+++ tank-war/src/terrains/elements.js
@@ -1,91 +1,80 @@
-import Terrains from "./terrains.js";
+import Terrains from './terrains.js'
 
-export class Element extends Terrains {
-  constructor(game, x, y, degree) {
-    super(game, x, y, degree);
-    this.frameY = 1;
-  }
-}
-export class Grass extends Element {
-  constructor(game, x, y, degree) {
-    super(game, x, y, degree);
-    this.frameX = 0;
-    this.type = "grass";
-  }
-}
-export class Wood extends Element {
-  constructor(game, x, y, degree) {
-    super(game, x, y, degree);
-    this.frameX = 1;
-    this.type = "wood";
-    this.randomDegree();
-  }
-}
-export class Tree extends Element {
-  constructor(game, x, y, degree) {
-    super(game, x, y, degree);
-    this.frameX = 2;
-    this.type = "tree";
-    this.getHit = false;
-    this.growTimer = 0;
-    this.growInterval = 10000;
-    this.randomDegree();
-  }
-  update(deltaTime) {
-    super.update();
-    // re-grow the tree after get hit
-    if (this.getHit) {
-      if (this.growTimer > this.growInterval) {
-        this.getHit = false;
-        this.growTimer = 0;
-      } else {
-        this.growTimer += deltaTime;
-      }
+export class Grass extends Terrains {
+    constructor(game, x, y, degree, type) {
+        super(game, x, y, degree, type)
+        this.frameX = 0
     }
+}
+export class Wood extends Terrains {
+    constructor(game, x, y, degree, type) {
+        super(game, x, y, degree, type)
+        this.frameX = 1
 
-    // collision tree - player
-    if (this.checkTerrainCollision(this.game.player)) {
-      this.getHit = true;
+        this.randomDegree()
     }
+}
+export class Tree extends Terrains {
+    constructor(game, x, y, degree, type) {
+        super(game, x, y, degree, type)
+        this.frameX = 2
 
-    // collision tree - enemies
-    this.game.enemies.forEach((enemy) => {
-      if (this.checkTerrainCollision(enemy)) {
-        this.getHit = true;
-      }
-    });
-    if (this.getHit) {
-      this.frameX = 1;
-    } else {
-      this.frameX = 2;
+        this.getHit = false
+        this.growTimer = 0
+        this.growInterval = 10000
+        this.randomDegree()
     }
-  }
+    update(deltaTime) {
+        super.update()
+        // re-grow the tree after get hit
+        if (this.getHit) {
+            if (this.growTimer > this.growInterval) {
+                this.getHit = false
+                this.growTimer = 0
+            } else {
+                this.growTimer += deltaTime
+            }
+        }
+
+        // collision tree - player
+        if (this.checkTerrainCollision(this.game.player)) {
+            this.getHit = true
+        }
+
+        // collision tree - enemies
+        this.game.enemies.forEach((enemy) => {
+            if (this.checkTerrainCollision(enemy)) {
+                this.getHit = true
+            }
+        })
+        if (this.getHit) {
+            this.frameX = 1
+        } else {
+            this.frameX = 2
+        }
+    }
 }
-export class Road extends Element {
-  constructor(game, x, y, degree) {
-    super(game, x, y, degree);
-    this.frameX = 3;
-    this.type = "rood";
-  }
+export class Road extends Terrains {
+    constructor(game, x, y, degree, type) {
+        super(game, x, y, degree, type)
+        this.frameX = 3
+    }
 }
-export class Turn extends Element {
-  constructor(game, x, y, degree) {
-    super(game, x, y, degree);
-    this.frameX = 4;
-    this.type = "turn";
-  }
+export class Turn extends Terrains {
+    constructor(game, x, y, degree, type) {
+        super(game, x, y, degree, type)
+        this.frameX = 4
+    }
 }
-export class T_Junction extends Element {
-  constructor(game, x, y, degree) {
-    super(game, x, y, degree);
-    this.frameX = 5;
-    this.type = "t_junction";
-  }
+export class T_Junction extends Terrains {
+    constructor(game, x, y, degree, type) {
+        super(game, x, y, degree, type)
+        this.frameX = 5
+    }
 }
-export class Crossroad extends Element {
-  constructor(game, x, y, degree) {
-    super(game, x, y, degree);
-    this.frameX = 6;
-    this.type = "crossroad";
-  }
+export class Crossroad extends Terrains {
+    constructor(game, x, y, degree, type) {
+        super(game, x, y, degree, type)
+        this.frameX = 6
+    }
 }
--- tank-war/src/terrains/terrains.js
+++ tank-war/src/terrains/terrains.js
@@ -1,36 +1,38 @@
 export default class Terrains {
-  constructor(game, x, y, degree) {
-    this.game = game;
-    this.image = terrainImage;
-    this.width = 128;
-    this.height = 128;
-    this.x = x;
-    this.y = y;
-    this.degree = degree;
-  }
-  checkTerrainCollision(object) {
-    return this.game.checkCircleCollision(this, object);
-  }
-  randomDegree() {
-    const degree = [0, 90, 180, 270];
-    this.degree = degree[Math.floor(Math.random() * degree.length)];
-  }
-  update() {}
-  draw(ctx) {
-    ctx.save();
-    ctx.translate(this.x + this.width * 0.5, this.y + this.height * 0.5);
-    ctx.rotate((this.degree * Math.PI) / 180);
-    ctx.drawImage(
-      this.image,
-      this.frameX * this.width,
-      this.frameY * this.height,
-      this.width,
-      this.height,
-      -this.width * 0.5,
-      -this.height * 0.5,
-      this.width,
-      this.height
-    );
-    ctx.restore();
-  }
+    constructor(game, x, y, degree, type) {
+        this.game = game
+        this.image = terrainImage
+        this.width = 128
+        this.height = 128
+        this.x = x
+        this.y = y
+        this.degree = degree
+        this.type = type
+        this.frameY = this.type
+    }
+    checkTerrainCollision(object) {
+        return this.game.checkCircleCollision(this, object)
+    }
+    randomDegree() {
+        const degree = [0, 90, 180, 270]
+        this.degree = degree[Math.floor(Math.random() * degree.length)]
+    }
+    update() {}
+    draw(ctx) {
+        ctx.save()
+        ctx.translate(this.x + this.width * 0.5, this.y + this.height * 0.5)
+        ctx.rotate((this.degree * Math.PI) / 180)
+        ctx.drawImage(
+            this.image,
+            this.frameX * this.width,
+            this.frameY * this.height,
+            this.width,
+            this.height,
+            -this.width * 0.5,
+            -this.height * 0.5,
+            this.width,
+            this.height
+        )
+        ctx.restore()
+    }
 }
