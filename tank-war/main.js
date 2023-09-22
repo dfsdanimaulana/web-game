ca3f5985a3fa4dbd628a8af067287df8ac5aa769
dfsdanimaulana <danimaulana9f@gmail.com>
2023-09-22 16:57:04 +0700

    fix(style): show and hide container when loading

--- tank-war/main.js
+++ tank-war/main.js
@@ -4,6 +4,7 @@ window.addEventListener("load", function () {
   const loading = document.getElementById("loading");
   loading.style.display = "none";
   const canvas = document.getElementById("container");
+  canvas.style.display = "block";
   const ctx = canvas.getContext("2d");
   canvas.width = 1280;
   canvas.height = 1280;
